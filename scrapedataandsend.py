ef33580811944619f09e00cbf352e6fbaf01162e
Ransu-ll <lancekyao@gmail.com>
2021-08-11 00:43:52 +1000

    Modify scrapedataandsend.py to clear old visualisations

--- scrapedataandsend.py
+++ scrapedataandsend.py
@@ -2,7 +2,8 @@ import scrapedata as scd
 import senddata as sed
 import visualisedata as vid
 
-scd.clear_old()
+scd.clear_old(scd.get_file_list(".csv"))
+scd.clear_old(scd.get_file_list(".png"), 3)
 scd.scrape_data(scd.fName)
 dataDate = scd.date_info(scd.fName)['dataDate']
 processedDate = scd.date_info(scd.fName)['processedDate']
