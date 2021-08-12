d9e4ab313ff68dba8dd66a771a09372a5611901a
Ransu-ll <lancekyao@gmail.com>
2021-08-12 12:47:14 +1000

    Move creation of lists into function to fix bug where visualisation would not be updated

--- visualisedata.py
+++ visualisedata.py
@@ -3,25 +3,26 @@
 from matplotlib import pyplot as plt
 import scrapedata as sd
 
-activeList = []
-newList = []
-dateList = []
-
-for file in sd.get_file_list(".csv"):
-    activeList.append(sd.take_info(file, sd.ColNames.ACTIVE_CASES, sd.Settings.SUM))
-    newList.append(sd.take_info(file, sd.ColNames.NEW_CASES, sd.Settings.SUM))
-    dateList.append(sd.date_info(file)["dataDate"])
-
 
 def create_visual():
     """Create visualisation based on number of new and total active cases"""
+
+    # Obtain file list
+    active_list = []
+    new_list = []
+    date_list = []
+
+    for file in sd.get_file_list(".csv"):
+        active_list.append(sd.take_info(file, sd.ColNames.ACTIVE_CASES, sd.Settings.SUM))
+        new_list.append(sd.take_info(file, sd.ColNames.NEW_CASES, sd.Settings.SUM))
+        date_list.append(sd.date_info(file)["dataDate"])
 
     # General plot details
     plt.title("COVID graph")
     plt.tick_params(axis='x', rotation=90)
     # Plotting information
-    active = plt.bar(dateList, activeList, label="active cases")
-    total = plt.bar(dateList, newList, label="new cases")
+    active = plt.bar(date_list, active_list, label="active cases")
+    total = plt.bar(date_list, new_list, label="new cases")
     plt.legend()  # Can only be put after data is plotted
     # Bar labels
     plt.bar_label(active, label_type="edge")
