4b6e947fbb8fc9147449e86dd279c460d8363c63
Ransu-ll <lancekyao@gmail.com>
2021-10-15 01:06:09 +1100

    Modify senddata.py to account for changes in the take_info() function

--- senddata.py
+++ senddata.py
@@ -6,8 +6,6 @@ import discord
 from dotenv import load_dotenv
 from typing import List
 
-import scrapedata as sd
-
 load_dotenv()
 
 # Webhook variables
@@ -75,22 +73,22 @@ def format_output(dict_list: List[dict], sep_post_value: str = ": ", sep: str = 
     return formatted_list
 
 
-def webhook(body: str, wrap_body: list = None,
-            command: sd.take_info = None, wrap_command: List[DiscordMarkup] = None,
+def webhook(body: str, wrap_body: List[DiscordMarkup] = None,
+            data: list = None, wrap_command: List[DiscordMarkup] = None,
             file: str = None, id_url: int = urlID,
             token_url: str = urlToken, wh_user: str = WHUsername,
             wh_avatar: str = WHAvatar, sep="\n"):
     """This is a POST request to any URL that supports webhooks.
 
-    `body` is the message that will be sent prior to `command`.
+    `body` is the message that will be sent prior to `data`.
     `wrap_body` is what the `body` should be wrapped in for formatting.
-    `command` is the command as specified by the takeInfo function.
-    `wrap_command` is similar to `wrapBody`, but for `command`.
+    `data` is the data as specified by the take_info function.
+    `wrap_command` is similar to `wrapBody`, but for `data`.
     `id_url` is the ID of the webhook link.
     `token_url` is the token of the webhook link.
     `wh_user` is the display name to be used.
     `wh_avatar` is the avatar to be displayed - must be a URL.
-    `sep` is the variable used to separate the `body` and `command`.
+    `sep` is the variable used to separate the `body` and `data`.
     """
 
     def wrap(self: List[DiscordMarkup]):
@@ -105,22 +103,22 @@ def webhook(body: str, wrap_body: list = None,
         # Wrap the body in something to format it for display.
         body = wrap(wrap_body) + body + wrap(wrap_body)[::-1]
 
-    if wrap_command and not command and command != 0:
+    if wrap_command and not data and data != 0:
         # Because there is no point in wrapping something that is
         # empty.
-        # The 0 is there in case the command returns 0, which is seen
+        # The 0 is there in case the data returns 0, which is seen
         # as "False", and 0 is a value that needs to be wrapped
         # around.
-        raise Exception("No command, yet there is a wrap_command variable")
+        raise Exception("No data, yet there is a wrap_command variable")
 
     if wrap_command:
-        # Wrap the command in something to format it for display.
-        command = wrap(wrap_command) + str(command) + wrap(wrap_command)[::-1]
+        # Wrap the data in something to format it for display.
+        data = wrap(wrap_command) + str(data) + wrap(wrap_command)[::-1]
 
-    if command:
-        # If there is a command, add it onto the body and separate it
+    if data:
+        # If there is data, add it onto the body and separate it
         # for use in the POST request.
-        body = body + sep + str(command)
+        body = body + sep + str(data)
 
     if file:
         with open(file=file, mode="rb") as f:
